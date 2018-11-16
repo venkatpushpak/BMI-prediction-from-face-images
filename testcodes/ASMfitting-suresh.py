3711dc646be083136af66a07a3fbe10a56df7f1b
sureshvairamuthu <sureshvairamuthu@gmail.com>
2018-11-15 22:34:16 -0800

    coordinates cut down to required points

--- testcodes/ASMfitting-suresh.py
+++ testcodes/ASMfitting-suresh.py
@@ -14,10 +14,11 @@ import os
 def ASMfitting(imgin):
     
     detector = dlib.get_frontal_face_detector() #Face detector
-    predictor = dlib.shape_predictor('./Data/dlibShapepredictor/shape_predictor_68_face_landmarks.dat') #Landmark identifier
+    predictor = dlib.shape_predictor('../Data/dlibShapepredictor/shape_predictor_68_face_landmarks.dat') #Landmark identifier
     
     frame = cv2.imread(imgin)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    reqpoints = [0,4,8,12,16,17,18,20,21,22,23,25,26,36,37,38,39,42,43,44,45,66]
 
     
     detection = detector(gray, 1)
@@ -26,15 +27,16 @@ def ASMfitting(imgin):
         shape1 = face_utils.shape_to_np(shape)
         (x, y, w, h) = face_utils.rect_to_bb(d)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        for (x, y) in shape1:
-            cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
+        for i,(x, y) in enumerate(shape1):
+            if(i in reqpoints):
+                cv2.circle(frame, (x, y), 2, (0, 0, 255), -1)
     cv2.imshow("Output", frame)
-    cv2.imwrite()
+    cv2.imwrite("fittedtst.jpeg",frame)
     cv2.waitKey(0)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #         break
 
 
 
-ASMfitting('./test-images/testimg.jpeg')
+ASMfitting('../test-images/testimg.jpeg')
 # ASMfitting('./test-images/tstimg1.jpeg')
